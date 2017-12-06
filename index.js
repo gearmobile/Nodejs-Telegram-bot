2cc2f95321570521cda385bbb78554c81153409b
doroninds <dmitriy435@gmail.com>
2017-12-06 17:47:44 +0400

    add reddit module

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const roll = require('./bot_modules/roll');
 const help = require('./bot_modules/help');
 const crypta = require('./bot_modules/crypta');
-const roulette = require('./bot_modules/roulette');
+const reddit = require('./bot_modules/reddit');
 const _env = require('./env');
 
 
@@ -32,3 +32,13 @@ bot.onText(/\/crypta/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, crypta.price);
 });
+
+
+
+bot.onText(/\/reddit (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  reddit.category = match[1];
+  bot.sendMessage(chatId, reddit.feeds);
+});
+
+bot.onText(/\/test (.+)/, (msg, match) => {bot.sendMessage(msg.chat.id, match[1]*2)});
